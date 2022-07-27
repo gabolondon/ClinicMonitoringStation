c73b880ef8ea6bf9e32a094b2d68a5441a9d1c85
gabo Londono <ing.gabriel.londono@gmail.com>
2022-07-26 23:22:02 -0500

    plantilla generada para datos de sensor, recogida d etemperatura exitosa en test.py pero aun falta aprende como corer el proceso en segundo plano para mostrarlo en la pagina !

--- TemperatureHumiditySensor/tempHumSensors/mainApp/tests.py
+++ TemperatureHumiditySensor/tempHumSensors/mainApp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+import time
+import paho.mqtt.client as mqtt
 # Create your tests here.
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+timetokill= 0
+client =mqtt.Client(client_id='Cliente3')
+client.on_message= on_message
+client.connect("192.168.1.109", port=1883)
+if client.is_connected:
+    print('cliente conectado')
+    client.subscribe("/temperatura")
+    
+    print("Subscribing to topic","/temperatura")
+else:
+    print('nada que conecta')
+while client.is_connected:
+    client.loop_start() #start the loop
+    #client.loop_stop() #stop the loop
